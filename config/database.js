9a07fcf2d9a88427b2ecffa50af79210e7dc360d
Juan Camilo Villafane <juangaav@gmail.com>
2024-12-17 14:57:48 -0500

    Fixed database config

--- config/database.js
+++ config/database.js
@@ -3,7 +3,18 @@ const { Sequelize } = require('sequelize');
 const env = process.env.NODE_ENV || 'development';
 const config = require('./config')[env];
 
-const sequelize = new Sequelize(config);
+//const sequelize = new Sequelize(config);
 //const sequelize = new Sequelize(config[env]);
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  });
 
 module.exports = sequelize;
